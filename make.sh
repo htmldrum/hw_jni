82d56a7d20f7b7556dee7bcf230e22dda9845ba8
htmldrum <htmldrum@tutanota.com>
2016-12-09 15:21:52 +1100

    .jnilib ext on OSX

--- make.sh
+++ make.sh
@@ -2,5 +2,5 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
 javac HelloWorld.java
 javah HelloWorld
-gcc -shared -fPIC -I $JAVA_HOME/include -I $JAVA_HOME/include/darwin libHelloWorld.c -o libHelloWorld.so
+gcc -shared -fPIC -I $JAVA_HOME/include -I $JAVA_HOME/include/darwin libHelloWorld.c -o libHelloWorld.jnilib
 java HelloWorld
