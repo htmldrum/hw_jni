27c6d3d67f82609558bb19a96045bebfa2514c80
htmldrum <htmldrum@tutanota.com>
2016-12-09 13:58:03 +1100

    JNI in C

---
+++ make.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
+javac HelloWorld.java
+javah HelloWorld
+gcc -shared -fPIC -I $JAVA_HOME/include -I $JAVA_HOME/include/darwin libHelloWorld.c -o libHelloWorld.so
+java HelloWorld
